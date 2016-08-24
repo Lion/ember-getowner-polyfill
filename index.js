7889bbbb0f4d27bf477fb3150b1fd87827f99a91
Kyle Nathan <kyle@wearelion.com>
2016-08-24 15:36:04 +0100

    Enable the babel loose flag
    Prevent Object.defineProperty usage for IE8

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-getowner-polyfill'
+  name: 'ember-getowner-polyfill',
+
+  init: function() {
+    this._super && this._super.init && this._super.init.apply(this, arguments);
+
+    this.options = this.options || {};
+    this.options.babel = this.options.babel || {};
+    this.options.babel.loose = true;
+  }
 };
